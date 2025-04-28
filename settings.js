2a53c68dc5779cf5857f81c2a0f9b8548f7a04de
Chibudenga <petercaleb.advocates@gmail.com>
2025-04-28 10:10:26 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'TEKIRO',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Tekiro Basmiber",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '+254701061390', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  commandMode: "private",
+  description: "This is my personal assistant",
   version: "1.0.0",
 };
 
